e8a3014bc7d9710799b366f28487d7525c27dece
Robert Ing <rmi22186@gmail.com>
2013-09-19 14:17:16 -0700

    add edit, show, destroy to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,23 +9,41 @@ class PostsController < ApplicationController
 	end
 
 	def new
-
+		@post = Post.new
+		@category = Category.all
 	end
 
 	def create
+		@post = Post.new(params[:post])
+
+
+		if @post.save
+			redirect_to posts_path, :notice => "Your post has been saved!"
+		else
+			render "new"
+		end
 
 	end
 
 	def edit
+		@post = Post.find(params[:id])
 
 	end
 
 	def update
+		@post = Post.find(params[:id])
 
+		if @post.update_attributes(params[:post])
+			redirect_to posts_path, :notice => "Your post has been updated!"
+		else
+			render "edit"
+		end
 	end
 
 	def destroy
-
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path, :notice => "Your post has been deleted"
 	end
 
 
