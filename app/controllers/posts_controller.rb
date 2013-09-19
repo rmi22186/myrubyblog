07f3ceee6a30d379c28063d2ed3a9259e03209bc
Robert Ing <rmi22186@gmail.com>
2013-09-19 00:41:39 -0700

    add posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,32 @@
 class PostsController < ApplicationController
 	def index
+		@content_first = "This is a sample post"
+		@content_second = "This is a sample post"
+	end
+
+	def new
 
 	end
+
+	def create
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def show
+
+	end
+
+	def destroy
+
+	end
+
 
 end
